ef23a3bccc38441940d429db2ebf2690493d7ecb
Pratham <Pratham.singla.762@gmail.com>
2023-05-22 18:13:23 +0530

    made structered directories

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();   //setup app using express server
+const port = 8000;       //port 
+
+app.use('/',require('./routes'));  //routing using express router 
+
+app.set('view engine','ejs');  //setting up view engine as ejs
+app.set('views','/views');    //views path directory
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error : ${err}`)
+        return;
+        }
+        console.log(`server is running on ${port}`);
+    });
